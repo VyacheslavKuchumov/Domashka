0bc24138cd32abf1236da314d8cde307184a4d7d
VyacheslavKuchumov <87779022+VyacheslavKuchumov@users.noreply.github.com>
2021-12-17 00:47:40 +0500

    Create stroki19.py

---
+++ AV/stroki19.py
@@ -0,0 +1,12 @@
+stroka = list(input("Введите строку:"))
+count = 0
+
+for i in range(len(stroka)-1, -1, -1):
+    if stroka[i] == ':':
+        stroka[i] = ''
+        count+=1
+
+
+print(''.join(stroka))
+print(f'Количество удаленных символов:{count}')
+
