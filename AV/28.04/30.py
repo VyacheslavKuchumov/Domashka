68aac92327dea8c60611ee2608deca2757e1a924
VyacheslavKuchumov <87779022+VyacheslavKuchumov@users.noreply.github.com>
2022-04-27 12:59:24 +0500

    Create 30.py

---
+++ AV/28.04/30.py
@@ -0,0 +1,22 @@
+text = input('Введите текст: ').lower()
+
+#text = 'Раз ДВА три работа танец товарищ'.lower() ###для проверки###
+count = 0
+RUN = True
+
+while(RUN):
+    c = input('Введите букву: ').lower()
+
+    for i in range(len(text)):
+        if i == 0:
+            if text[i] == c:
+                count+=1
+
+        else:
+            if text[i] == c and text[i-1] == ' ':
+                count +=1
+
+    print(f"Количество слов, начинающихся с указанной буквы: {count}")
+    prompt = input('Чтобы выйти пропишите \"quit\"\n').lower()
+    if prompt == 'quit':
+        RUN =False
