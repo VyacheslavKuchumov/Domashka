46608d8f6df624fd7f4effcc0598d0b8dccc607a
VyacheslavKuchumov <87779022+VyacheslavKuchumov@users.noreply.github.com>
2022-11-22 13:55:38 +0500

    Update program1.8.cs

--- kuzaev/LaBA 5/program1.8.cs
+++ kuzaev/LaBA 5/program1.8.cs
@@ -1,1 +1,38 @@
-bruh
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyFirstProgram
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> listOfStrings = new List<string>();
+            DoingStuff.AddElement("The Bends", listOfStrings);
+            DoingStuff.AddElement("In Rainbows", listOfStrings);
+            DoingStuff.AddElement("Kid A", listOfStrings);
+            DoingStuff.PrintList(listOfStrings);
+        }
+    }
+    public class DoingStuff
+    {
+        public static void AddElement(string text, List<string> list)
+        {
+            list.Add(text);
+        }
+
+        public static void PrintList(List<string> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }    
+        }
+    }
+    
+
+}
